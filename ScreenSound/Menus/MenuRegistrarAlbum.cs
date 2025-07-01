afe183831e5c3fe1926a58fbbcee36420ada2c44
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2025-07-01 20:43:18 -0300

    Refactor menu logic into modular classes
    Refactored menu options into dedicated classes under the
    ScreenSound.Menus namespace, improving modularity and
    separation of concerns. Introduced a base `Menu` class for
    reusable functionality and updated `Program.cs` to use these
    classes, enhancing readability and maintainability. Removed
    redundant inline methods and centralized logic in menu-specific
    classes. Preserved existing functionality and user experience,
    while improving scalability for future enhancements.

---
+++ ScreenSound/Menus/MenuRegistrarAlbum.cs
@@ -0,0 +1,37 @@
+﻿using ScreenSound.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Menus;
+internal class MenuRegistrarAlbum : Menu
+{
+    public void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        Console.Clear();
+        ExibirTituloDaOpcao("Registro de álbuns");
+        Console.Write("Digite a banda cujo álbum deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Console.Write("Agora digite o título do álbum: ");
+
+        if (bandasRegistradas.ContainsKey(nomeDaBanda))
+        {
+            Banda banda = bandasRegistradas[nomeDaBanda];
+
+            string tituloAlbum = Console.ReadLine()!;
+            banda.AdicionarAlbum(new Album(tituloAlbum, banda));
+            Console.WriteLine($"O álbum {tituloAlbum} de {nomeDaBanda} foi registrado com sucesso!");
+            Thread.Sleep(4000);
+            Console.Clear();
+        }
+        else
+        {
+            Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
+            Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
